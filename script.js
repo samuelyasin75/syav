9b8629b324b379d6c2f7f50544b6dbfb400166da
S2mu8l <S2mu8l@gmail.com>
2025-04-20 18:54:17 -0300

    Comprimento e design de notas

--- script.js
+++ script.js
@@ -1,3 +1,41 @@
-document.getElementsByTagName('body')[0].addEventListener('load', function() {
-    alert('funcionafadf')
-})
+function comprimentar() {
+    var data = new Date()
+    var horas = data.getHours()
+    var minutos = data.getMinutes()
+    var diaSemana = data.getDay()
+
+    switch (diaSemana) {
+        case 0:
+            diaSemana = 'Domingo'
+        break
+        case 1:
+            diaSemana = 'Segunda-feira'
+        break
+        case 2:
+            diaSemana = 'Terça-feira'
+        break
+        case 3:
+            diaSemana = 'Quarta-feira'
+        break
+        case 4:
+            diaSemana = 'Quinta-feira'
+        break
+        case 5:
+            diaSemana = 'Sexta-feira'
+        break
+        case 6:
+            diaSemana = 'Sábado'
+        break
+    }
+
+    var comprimento
+    if (horas >= 0 && horas <= 11) {
+        comprimento = 'Bom dia!'
+    } else if (horas >= 12 && horas <= 18) {
+        comprimento = 'Boa tarde!'
+    } else {
+        comprimento = 'Boa noite!'
+    }
+
+    document.getElementById('msg').innerHTML = `<h1>${comprimento}</h1><p>Acesso feito: ${horas + ':' + minutos}<br>${diaSemana}</p>`
+}
